f27412cecbf4e1e4280f4731bfe8233c8d08debe
Jojo <jfrasier96@gmail.com>
2023-03-02 18:27:17 -0500

    first commit

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+function GetInfo() {
+  const newName = document.getElementById("cityInput");
+  const cityName = document.getElementById("cityName");
+  cityName.innerHTML = "--" + newName.value + "--";
+}
+
+fetch;
+"https://api.openweathermap.org/data/2.5/weather?q=%27+city + " & appid;
+" + f5e714a68db3e76d8aa2c2377b0726f2".then((response) => response.json());
