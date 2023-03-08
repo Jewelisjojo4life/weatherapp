945c7e4c7301d8939f19f5d15dd9d18c00dc55ae
Jojo <jfrasier96@gmail.com>
2023-03-07 19:45:31 -0500

    updated files

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,45 @@
-function GetInfo() {
-  const newName = document.getElementById("cityInput");
-  const cityName = document.getElementById("cityName");
-  cityName.innerHTML = "--" + newName.value + "--";
-}
+const weatherURL = "https://api.openweathermap.org/data/2.5/forecast?";
+const lat = "&lat=33.753746";
+const lon = "&lon=-84.386330";
+const apiOptions = "&units=imperial&exclude=minutelyalerts&appid=";
+const apiKey = "f5e714a68db3e76d8aa2c2377b0726f2";
+const url = weatherURL + lat + lon + apiOptions + apiKey;
+const button = document.getElementById("GetInfo");
 
-fetch;
-"https://api.openweathermap.org/data/2.5/weather?q=%27+city + " & appid;
-" + f5e714a68db3e76d8aa2c2377b0726f2".then((response) => response.json());
+function GetInfo() {
+  var newName = document.getElementById("cityInput");
+  var cityName = document.getElementById("cityName");
+  cityName.innerHTML = "--" + newName.value + "--";
+
+  fetch(
+    (geoUrl =
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      newName.value +
+      "&units=imperial&exclude=minutelyalerts&appid=f5e714a68db3e76d8aa2c2377b0726f2")
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      let day = 1;
+
+      for (let index = 0; index < data.list.length; index += 8) {
+        const wind = data.list[index].wind.speed;
+        const temp = data.list[index].main.temp;
+        const humidity = data.list[index].main.humidity;
+
+        const id = "day" + day;
+        day++;
+        const element = document.getElementById(id);
+
+        element.innerText =
+          "  Temp : " +
+          temp +
+          +"  windspeed:" +
+          wind +
+          "  humidity:  " +
+          "," +
+          humidity;
+      }
+    });
+}
